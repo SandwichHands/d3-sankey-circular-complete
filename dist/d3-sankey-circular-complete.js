301105616c53a888e150ce005568b186ed9afb89
SandwichHands <bryhelleso@gmail.com>
2024-08-18 12:18:56 -0500

    updated ascendingBreadth

--- dist/d3-sankey-circular-complete.js
+++ dist/d3-sankey-circular-complete.js
@@ -177,17 +177,8 @@
       return graph;
     } // end of sankeyCircular function
 
-    // sort links' breadth (ie top to bottom in a column), based on their source nodes' breadths
-    function ascendingSourceBreadth(a, b) {
-      return ascendingBreadth(a.source, b.source) || a.index - b.index;
-    }
-
-    // sort links' breadth (ie top to bottom in a column), based on their target nodes' breadths
-    function ascendingTargetBreadth(a, b) {
-      return ascendingBreadth(a.target, b.target) || a.index - b.index;
-    }
-
-    // sort nodes' breadth (ie top to bottom in a column)
+    
+// sort nodes' breadth (ie top to bottom in a column)
     // if both nodes have circular links, or both don't have circular links, then sort by the top (y0) of the node
     // else push nodes that have top circular links to the top, and nodes that have bottom circular links to the bottom
     function ascendingBreadth(a, b) {
@@ -202,6 +193,17 @@
         }
       }
     }
+    // sort links' breadth (ie top to bottom in a column), based on their source nodes' breadths
+    function ascendingSourceBreadth(a, b) {
+      return ascendingBreadth(a.source, b.source) || a.index - b.index;
+    }
+
+    // sort links' breadth (ie top to bottom in a column), based on their target nodes' breadths
+    function ascendingTargetBreadth(a, b) {
+      return ascendingBreadth(a.target, b.target) || a.index - b.index;
+    }
+
+    
 
     // Set the sankeyCircular parameters
     // nodeID, nodeAlign, nodeWidth, nodePadding, nodes, links, size, extent, iterations, nodePaddingRatio, circularLinkGap
